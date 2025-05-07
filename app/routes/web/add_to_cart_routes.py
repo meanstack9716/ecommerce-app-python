41be32699a8ff8dc5bb8609be98f239cd35d5b98
Mean Stack <meanstack9@gmail.com>
2025-05-07 09:11:42 +0530

    created a checkout page

--- app/routes/web/add_to_cart_routes.py
+++ app/routes/web/add_to_cart_routes.py
@@ -2,7 +2,16 @@ from flask import render_template, session, redirect, url_for, request
 from . import admin_api
 
 
-@admin_api.route('/cart')
+@admin_api.route('/cart', methods=['GET', 'POST'])
 def view_cart():
-    cart = session.get('cart', [])
-    return render_template('admin/addToCart/cart.html', cart=cart)
+    if 'user_id' not in session:
+        return redirect(url_for('admin_api.login_page'))
+    return render_template('admin/addToCart/cart.html')
+
+
+
+@admin_api.route('/checkout', methods=['GET', 'POST'])
+def checkout_page():
+    if 'user_id' not in session:
+        return redirect(url_for('admin_api.login_page'))
+    return render_template('admin/addToCart/checkout.html')
